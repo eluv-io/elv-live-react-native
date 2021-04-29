60dfb2ab661398a858fd25583e4fe59f0c48df6d
elv-wayne <wayne.tran@eluv.io>
2021-04-29 12:41:02 -0400

    update build to 1.0.43

--- App.js
+++ App.js
@@ -42,7 +42,7 @@ import DeviceInfo from 'react-native-device-info';
 import uuid from 'react-native-uuid';
 
 const APP_STORAGE_KEY = "@eluvio_live";
-const APP_VERSION = "1.0.42";
+const APP_VERSION = "1.0.43";
 
 const isHermes = () => !!global.HermesInternal;
 
